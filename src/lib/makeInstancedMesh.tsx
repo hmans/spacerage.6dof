86e33f3e905eab9e9f5a2376105f8cf2bd967f64
Hendrik Mans <hendrik@mans.de>
2022-05-05 21:32:31 +0200

    Use matrix4 in instanza

---
+++ src/lib/makeInstancedMesh.tsx
@@ -0,0 +1,169 @@
+import {
+  GroupProps,
+  InstancedMeshProps,
+  Object3DProps,
+  useFrame
+} from "@react-three/fiber"
+import { IEntity, RegisteredEntity, Tag, World } from "miniplex"
+import { createECS } from "miniplex-react"
+import React, {
+  FC,
+  forwardRef,
+  useEffect,
+  useMemo,
+  useRef,
+  useState
+} from "react"
+import {
+  BufferGeometry,
+  DynamicDrawUsage,
+  Group,
+  InstancedMesh,
+  Material,
+  Matrix4,
+  Object3D,
+  Usage
+} from "three"
+import { Update } from "../spacerage/Update"
+
+export type InstanceComponents = {
+  instance: Tag
+  matrix4: Matrix4
+  visible: boolean
+}
+
+export type InstanceEntity<CustomComponents = IEntity> = CustomComponents &
+  InstanceComponents
+
+export const makeInstancedMesh = <Custom extends IEntity = IEntity>({
+  systemFactory,
+  entityFactory,
+  material,
+  geometry,
+  usage = DynamicDrawUsage
+}: {
+  entityFactory?: () => InstanceEntity<Custom>
+  systemFactory?: (world: World<InstanceEntity<Custom>>) => (dt: number) => void
+  material?: Material | Material[]
+  geometry?: BufferGeometry
+  usage?: Usage
+} = {}) => {
+  /* We're using Miniplex as a state container. */
+  const ECS = createECS<InstanceEntity<Custom>>()
+
+  /* If a system factory has been passed, prepare the custom system. */
+  const system = systemFactory && systemFactory(ECS.world)
+
+  /* This component renders the InstancedMesh itself and continuously updates it
+     from the data in the ECS. */
+  const Root: FC<
+    InstancedMeshProps & {
+      instanceLimit?: number
+    }
+  > = ({ instanceLimit = 10000, ...props }) => {
+    const instancedMesh = useRef<InstancedMesh>(null!)
+
+    useEffect(() => {
+      instancedMesh.current.instanceMatrix.setUsage(usage)
+    }, [])
+
+    const { entities } = ECS.useArchetype("matrix4", "visible")
+
+    function updateInstanceMatrix() {
+      const imesh = instancedMesh.current
+      const l = entities.length
+      let count = 0
+
+      for (let i = 0; i < l; i++) {
+        const { matrix4, visible } = entities[i]
+
+        if (visible) {
+          imesh.setMatrixAt(i, matrix4)
+          count++
+        }
+      }
+
+      imesh.instanceMatrix.needsUpdate = true
+      imesh.count = count
+    }
+
+    useFrame((_, dt) => {
+      system?.(dt)
+      updateInstanceMatrix()
+    }, Update.Late)
+
+    return (
+      <instancedMesh
+        {...props}
+        ref={instancedMesh}
+        args={[geometry, material, instanceLimit]}
+      />
+    )
+  }
+
+  const useEntities = (
+    count = 1,
+    entityFactory: () => InstanceEntity<Custom>
+  ) => {
+    const [entities] = useState(() => {
+      const entities = new Array<InstanceEntity<Custom>>()
+
+      for (let i = 0; i < count; i++) {
+        entities.push({
+          ...(entityFactory ? entityFactory() : ({} as InstanceEntity<Custom>))
+        })
+      }
+
+      return entities
+    })
+
+    useEffect(() => {
+      for (const entity of entities) {
+        ECS.world.createEntity(entity)
+      }
+
+      return () => {
+        for (const entity of entities) {
+          ECS.world.destroyEntity(
+            entity as RegisteredEntity<InstanceEntity<Custom>>
+          )
+        }
+      }
+    })
+
+    return entities
+  }
+
+  const useInstances = (count = 1) =>
+    useEntities(count, () => ({
+      instance: Tag,
+      matrix4: new Matrix4(),
+      visible: true,
+      ...(entityFactory ? entityFactory() : ({} as Custom))
+    }))
+
+  const Instance = forwardRef<Group, Object3DProps>((props, ref) => {
+    const instance = useInstances(1)[0]
+    const localRef = useRef<Object3D>(null!)
+
+    useEffect(() => {
+      instance.matrix4 = localRef.current.matrixWorld
+    })
+
+    return <object3D matrix={instance.matrix4} {...props} ref={localRef} />
+  })
+
+  const ThinInstance: FC<{ count?: number }> = ({ count = 1 }) => {
+    useInstances(count)
+    return null
+  }
+
+  return {
+    world: ECS.world,
+    useArchetype: ECS.useArchetype,
+    useInstances,
+    Root,
+    Instance,
+    ThinInstance
+  }
+}
--- src/spacerage/Game.tsx
+++ src/spacerage/Game.tsx
@@ -1,18 +1,49 @@
 import { Canvas, useFrame } from "@react-three/fiber"
+import { plusMinus } from "randomish"
+import { useMemo } from "react"
+import { Matrix4, Object3D, Vector3 } from "three"
+import { makeInstancedMesh } from "../lib/makeInstancedMesh"
 import { ECS } from "./ecs"
 import { Update } from "./Update"
+
+const Swarm = () => {
+  const Blob = useMemo(
+    () =>
+      makeInstancedMesh({
+        entityFactory: () => {
+          const m4 = new Matrix4()
+          m4.setPosition(plusMinus(3), plusMinus(3), plusMinus(3))
+          return {
+            matrix4: m4
+          }
+        }
+      }),
+    []
+  )
+
+  return (
+    <Blob.Root>
+      <sphereGeometry args={[0.1]} />
+      <meshBasicMaterial color="orange" />
+
+      <Blob.ThinInstance count={1000} />
+    </Blob.Root>
+  )
+}
 
 export const Game = () => {
   return (
     <Canvas>
-      <ECS.Entity>
+      <Swarm />
+
+      {/* <ECS.Entity>
         <ECS.Component name="transform">
           <mesh>
             <boxBufferGeometry />
             <meshBasicMaterial color="hotpink" />
           </mesh>
         </ECS.Component>
-      </ECS.Entity>
+      </ECS.Entity> */}
 
       <Systems />
     </Canvas>
