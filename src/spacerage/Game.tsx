2b0f4bdd0f1111ed0cbeb780176fe4255b37d471
Hendrik Mans <hendrik@mans.de>
2022-05-05 20:45:01 +0200

    ECS things up

--- src/spacerage/Game.tsx
+++ src/spacerage/Game.tsx
@@ -1,12 +1,31 @@
-import { Canvas } from "@react-three/fiber";
+import { Canvas, useFrame } from "@react-three/fiber"
+import { ECS } from "./ecs"
 
 export const Game = () => {
   return (
     <Canvas>
-      <mesh>
-        <boxBufferGeometry />
-        <meshBasicMaterial color="hotpink" />
-      </mesh>
+      <ECS.Entity>
+        <ECS.Component name="transform">
+          <mesh>
+            <boxBufferGeometry />
+            <meshBasicMaterial color="hotpink" />
+          </mesh>
+        </ECS.Component>
+      </ECS.Entity>
+
+      <Systems />
     </Canvas>
-  );
-};
+  )
+}
+
+const Systems = () => {
+  const { entities } = ECS.useArchetype("transform")
+
+  useFrame((_, dt) => {
+    for (const { transform } of entities) {
+      transform.rotation.x = transform.rotation.y += 1.5 * dt
+    }
+  })
+
+  return null
+}
