1dd9e4c668abd47e9ba9b3fbecc0ccb7f08f5838
Hendrik Mans <hendrik@mans.de>
2022-05-06 23:22:22 +0200

    Cleanup

--- src/spacerage/Game.tsx
+++ src/spacerage/Game.tsx
@@ -9,32 +9,30 @@ import { RenderPipeline } from "./RenderPipeline"
 import { Skybox } from "./Skybox"
 import { Systems } from "./systems/Systems"
 
-export const Game = () => {
-  return (
-    <Canvas
-      flat
-      gl={{
-        logarithmicDepthBuffer: true,
-        outputEncoding: LinearEncoding,
-        alpha: false,
-        stencil: false,
-        antialias: true
-      }}
-    >
-      <fog args={["#000", 0, 800]} attach="fog" />
-      <ambientLight intensity={0.2} />
-      <directionalLight intensity={2} position={[300, 100, -200]} />
-      <RenderPipeline />
-      <Skybox />
+export const Game = () => (
+  <Canvas
+    flat
+    gl={{
+      logarithmicDepthBuffer: true,
+      outputEncoding: LinearEncoding,
+      alpha: false,
+      stencil: false,
+      antialias: true
+    }}
+  >
+    <fog args={["#000", 0, 800]} attach="fog" />
+    <ambientLight intensity={0.2} />
+    <directionalLight intensity={2} position={[300, 100, -200]} />
+    <RenderPipeline />
+    <Skybox />
 
-      <PhysicsWorld gravity={[0, 0, 0]}>
-        <Camera />
-        <Player />
-        <Asteroids />
-        <Bullets />
+    <PhysicsWorld gravity={[0, 0, 0]}>
+      <Camera />
+      <Player />
+      <Asteroids />
+      <Bullets />
 
-        <Systems />
-      </PhysicsWorld>
-    </Canvas>
-  )
-}
+      <Systems />
+    </PhysicsWorld>
+  </Canvas>
+)
