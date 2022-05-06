264e122785d82a33a61d224d257527312e89d150
Hendrik Mans <hendrik@mans.de>
2022-05-06 19:21:03 +0200

    useInstancedGLTF

---
+++ src/lib/instanza/useInstancedGLTF.ts
@@ -0,0 +1,13 @@
+import { useGLTF } from "@react-three/drei"
+import { Mesh } from "three"
+import { useInstancedMesh } from "./useInstancedMesh"
+
+export const useInstancedGLTF = (url: string) => {
+  const gltf = useGLTF(url)
+  const mesh = gltf.scene.children[0] as Mesh
+
+  return {
+    ...useInstancedMesh({ geometry: mesh.geometry, material: mesh.material }),
+    mesh
+  }
+}
