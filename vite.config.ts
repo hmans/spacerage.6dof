15e056e117ef4b2ac2301fd456396bf8c5cfb25f
Hendrik Mans <hendrik@mans.de>
2022-05-06 20:24:05 +0200

    Ignore some deps for better local testing

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  optimizeDeps: {
+    exclude: [
+      "miniplex",
+      "miniplex-react",
+      "@hmans/controlfreak",
+      "@hmans/signal",
+      "@hmans/ui"
+    ],
+    include: ["react/jsx-runtime"]
+  }
 })
