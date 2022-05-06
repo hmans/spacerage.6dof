c9a412226c152473f14bbd7a80d81043f045d239
Hendrik Mans <hendrik@mans.de>
2022-05-06 19:08:05 +0200

    Load Rapier first :-)

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { App } from "./App";
-import "./index.css";
+import React from "react"
+import ReactDOM from "react-dom/client"
+import { App } from "./App"
+import "./index.css"
+import * as RAPIER from "@dimforge/rapier3d-compat"
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+RAPIER.init().then(() =>
+  ReactDOM.createRoot(document.getElementById("root")!).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  )
+)
